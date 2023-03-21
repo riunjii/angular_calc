3b1775136d82a255f9306ca5e018e8c9254a7aa1
riunjii <vitor2002_souza@hotmail.com>
2023-03-21 11:20:09 -0300

    bckp

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent {
+  //atributos da classe/componente
+num1 : number = 0;
+num2 : number = 0;
+soma : number = 0;
+
+  somar(){
+
+  this.soma = this.num1 + this.num2 
+
+  }
+
+}
